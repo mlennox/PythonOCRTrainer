d501deb9ff6d517806b1a2381a550c075e7c8986
mlennox <mark@webpusher.ie>
2016-02-17 23:46:55 +0000

    ok, finally twigged that gpos or kerning IS being applied

--- boundingboxes/index.js
+++ boundingboxes/index.js
@@ -7,7 +7,7 @@ var fse = require('fs-extra');
 var gposLookupType = ['undefined', 'Single Adjustment', 'Pair Adjustment', 'Cursive Attachment', 'MarkToBase Attachment', 'MarkToLigature Attachment', 'MarkToMark Attachment', 'Context Positioning', 'Chained Context Positioning', 'Extension Positioning'];
 
 // we will want to load the texts too
-var example_text = "FiFM"
+var example_text = "FiiFPiiPMiiMNiiN"
 
 function fetch_fonts() {
 	console.log('going to fetch fonts');
@@ -24,23 +24,54 @@ function fetch_fonts() {
 				resolve(fonts);
 			});
 	});
+}
 
+// https://www.microsoft.com/typography/otspec/chapter2.htm
+
+/**
+ * [single_adjustment description]
+ * @param  {[type]} index_offset [description]
+ * @param  {[type]} subTable     [description]
+ * @return {[type]}              [description]
+ */
+function single_adjustment(index_offset, subTable) {
+	console.log('Single Adjustment - - - - - - -');
+	console.log(subTable[index_offset]);
+}
+
+/**
+ * [pair_adjustment_version_1 description]
+ * @param  {[type]} glyphId      [description]
+ * @param  {[type]} nextGlyphId  [description]
+ * @param  {[type]} index_offset [description]
+ * @param  {[type]} subTable     [description]
+ * @return {[type]}              [description]
+ */
+function pair_adjustment_version_1(glyphId, nextGlyphId, index_offset, subTable) {
+
+}
+
+/**
+ * 
+ * @param  {[type]} index_offset [description]
+ * @param  {[type]} subTable     [description]
+ * @return {[type]}              [description]
+ */
+function pair_adjustment_version_2(glyphId, nextGlyphId, index_offset, subTable) {
+	// offset has the form 
+	// 		{ version: 2, classRangeCount: 217, 
+	// 		  classRangeRecord: [
+	// 		  	// the start/end refers to the range of glyphIds
+	//  		{ start: 16, end: 16, class: 1 },
+	//   		{ start: 17, end: 17, class: 2 },
+	//   		etc.
+	//   	  ]
+	//   	}
+	// classDef1 is the lookup for the first Glyph for subTable.classRecords table
+	// classDef2 is the lookup for the second Glyph for subTable.classRecords table
 	
-
-	// return fonts;
-}
-
-function single_adjustment() {
-
-}
-
-
-function pair_adjustment_version_1() {
-
-}
-
-function pair_adjustment_version_2() {
-	
+	console.log('pair adjustment classDef');
+	console.log(subTable);
 }
 
 /**
@@ -58,9 +89,10 @@ function convert_coverage(glyphId, nextGlyphId, lookupType, coverage, subTable) 
 
 	console.log('convert_coverage =-=-=-=-=-=-=-=-=-=-');
 
-	if (lookupType === 2 && nextGlyphId === null){
-		console.log('pair set lookup, but no next glyph - we are done');
-	}
+	// this is not necessarily true
+	// if (lookupType === 2 && nextGlyphId === null){
+	// 	console.log('pair set lookup, but no next glyph - we are done');
+	// }
 
 	// find the glyph id in the coverage list
 	// 		if it does not exist then no further calculation needed for this lookup table for this glyph
@@ -90,8 +122,8 @@ function convert_coverage(glyphId, nextGlyphId, lookupType, coverage, subTable) 
 				break;
 			case 2:
 				(subTable.version === 1) 
-					? pair_adjustment_version_1(index_offset, subTable) 
-					: pair_adjustment_version_2();
+					? pair_adjustment_version_1(glyphId, nextGlyphId, index_offset, subTable) 
+					: pair_adjustment_version_2(glyphId, nextGlyphId, index_offset, subTable);
 				break;
 			default:
 				console.log('Lookup not yet supported : ', gposLookupType[lookupType]);
@@ -99,133 +131,176 @@ function convert_coverage(glyphId, nextGlyphId, lookupType, coverage, subTable) 
 
 
 
-		// yeah its a big if statement, so sue me...
-		if (lookupType === 1){
+		// // yeah its a big if statement, so sue me...
+		// if (lookupType === 1){
 
-			console.log('Single Adjustment - - - - - - -');
-			console.log(subTable[index_offset]);
+		// 	console.log('Single Adjustment - - - - - - -');
+		// 	console.log(subTable[index_offset]);
 
-		} else if (lookupType === 2){
+		// } else if (lookupType === 2){
 
-			console.log('Pair Adjustment - - - - - - - - next glyph :', nextGlyphId);
+		// 	console.log('Pair Adjustment - - - - - - - - next glyph :', nextGlyphId);
 
-			console.log('subtable version', subTable.version);
+		// 	console.log('subtable version', subTable.version);
 			
-			if (subTable.version === 1){
-				console.log('pair sets');
-				// pairSets is of type - restructure:LazyArray
-				// { secondGlyph: 111, value1: [Object], value2: {} },
-				var pair_sets = subTable.pairSets.get(index_offset);
-				var pair = _.filter(pair_sets, function(pair_set){
-					console.log('pair_set', pair_set);
-					return pair_set.secondGlyph === nextGlyphId;
-				});
-				console.log('pair', pair);
-			} else if (subTable.version === 2) {
-				console.log('classDef');
+		// 	if (subTable.version === 1){
+		// 		console.log('pair sets');
+		// 		// pairSets is of type - restructure:LazyArray
+		// 		// { secondGlyph: 111, value1: [Object], value2: {} },
+		// 		var pair_sets = subTable.pairSets.get(index_offset);
+		// 		var pair = _.filter(pair_sets, function(pair_set){
+		// 			console.log('pair_set', pair_set);
+		// 			return pair_set.secondGlyph === nextGlyphId;
+		// 		});
+		// 		console.log('pair', pair);
+		// 	} else if (subTable.version === 2) {
+		// 		console.log('classDef');
 
-				// offset has the form 
-				// 		{ version: 2, classRangeCount: 217, 
-				// 		  classRangeRecord: [
-				// 		  	// the start/end refers to the range of glyphIds
-				//  		{ start: 16, end: 16, class: 1 },
-				//   		{ start: 17, end: 17, class: 2 },
-				//   		etc.
-				//   	  ]
-				//   	}
-				// classDef1 is the lookup for the first Glyph for subTable.classRecords table
-				// classDef2 is the lookup for the second Glyph for subTable.classRecords table
-				// console.log('class def 1 offset', subTable.classDef1);
-				// console.log('class def 2 offset', subTable.classDef2);
-				// console.log('class records', subTable.classRecords);
-				var class_def_1 = _.filter(subTable.classDef1.classRangeRecord, function(def1) {
-					return def1.start <= glyphId && def1.start >= glyphId;
-				});
+		// 		// offset has the form 
+		// 		// 		{ version: 2, classRangeCount: 217, 
+		// 		// 		  classRangeRecord: [
+		// 		// 		  	// the start/end refers to the range of glyphIds
+		// 		//  		{ start: 16, end: 16, class: 1 },
+		// 		//   		{ start: 17, end: 17, class: 2 },
+		// 		//   		etc.
+		// 		//   	  ]
+		// 		//   	}
+		// 		// classDef1 is the lookup for the first Glyph for subTable.classRecords table
+		// 		// classDef2 is the lookup for the second Glyph for subTable.classRecords table
+		// 		// console.log('class def 1 offset', subTable.classDef1);
+		// 		// console.log('class def 2 offset', subTable.classDef2);
+		// 		// console.log('class records', subTable.classRecords);
+		// 		var class_def_1 = _.filter(subTable.classDef1.classRangeRecord, function(def1) {
+		// 			return def1.start <= glyphId && def1.start >= glyphId;
+		// 		});
 				
-				var class_def_2 = _.filter(subTable.classDef2.classRangeRecord, function(def2) {
-					return def2.start <= nextGlyphId && def2.start >= nextGlyphId;
-				});
+		// 		var class_def_2 = _.filter(subTable.classDef2.classRangeRecord, function(def2) {
+		// 			return def2.start <= nextGlyphId && def2.start >= nextGlyphId;
+		// 		});
 
 				
 				
 
-				// console.log('class 2 records', subTable.class2Record);
-				// classDef offsets are lookup tables to find which class a given glyph belongs to
-				// that class is then used to lookup the 
-			}
+		// 		// console.log('class 2 records', subTable.class2Record);
+		// 		// classDef offsets are lookup tables to find which class a given glyph belongs to
+		// 		// that class is then used to lookup the 
+		// 	}
 
 			
-		} else {
-			console.log('Lookup not yet supported : ', gposLookupType[lookupType]);
-		}
+		// } else {
+		// 	console.log('Lookup not yet supported : ', gposLookupType[lookupType]);
+		// }
 	}
 
 	return result;
 }
 
 function show_font_details(font) {
-	console.log('showing details of font');
+	console.log(example_text);
 
 	var lg = fk.openSync(font);
 
 	var lay = lg.layout(example_text);
 
-	// console.log(lay.glyphs);
+	console.log('-------------', font);
 
-	console.log('-------------------------------------------------------------');
-	for (var gid in lay.glyphs){
-		gid = gid * 1; // convert to int...
-		console.log('GLYPH =============', gid, lay.glyphs.length);
-		var glyph = lay.glyphs[gid];
-		var nextGlyphId = (gid < lay.glyphs.length) ? lay.glyphs[gid+1].id : null;
-		console.log(glyph.path.bbox);
-		console.log(glyph.id);
-		console.log(glyph._metrics);
+	// var ks = _.keys(lay);
+	// var result = [];
 
-		console.log('FONT -=-=-=-=-=-=-=-');
-		var gpos = glyph._font._tables.GPOS;
-		console.log('GPOS : ', gpos);
-		var lookupList = gpos.lookupList.items[0];
-		for (var lookup of lookupList.subTables){
-			console.log('COVERAGE : ',lookup.coverage);
-			console.log('type: ', gposLookupType[lookupList.lookupType]);
-			console.log('lookup: ', convert_coverage(glyph.id, nextGlyphId, lookupList.lookupType, lookup.coverage, lookup));
-		}
-		// console.log('Lookups : ',thing._font._tables.GPOS);
-	}
-	console.log('-------------------------------------------------------------');
+	// for (var k of ks){
+	// 	var kks = _.keys(k);
+	// 	console.log(kks)
+	// 	if (kks.length > 0){
+	// 		newk = {};
+	// 		newk[k] = kks;
+	// 		k = newk;
+	// 	}
+	// 	result.push(k);
+	// }
 
-	// https://www.microsoft.com/typography/otspec/gpos.htm
-
-	// returns a restructure/Struct
-	// not sure how to access the properties yet
-	// var thing = lay.glyphs[0]._font._tables.GPOS.lookupList;
-
-	// var tables = lay.glyphs[0]._font._tables;
-	// var gpos = tables.GPOS;
-
-	// var scriptList = gpos.scriptList;
-	// console.log('script list', scriptList);
-
-	// var featureList = gpos.featureList;
-	// console.log('feature list', featureList);
-	// console.log('feature list, lookup list indices', featureList[0].feature.lookupListIndexes);
+	// console.log('all keys',result);
 
 
-	// // lookup list tables
-	// // https://www.microsoft.com/typography/otspec/chapter2.htm
+	console.log(_.map(lay.glyphs, function(glyph) {
+		return {
+			kys: _.keys(glyph),
+			id: glyph.id, 
+			m: glyph._metrics, 
+			bbox: glyph.path.bbox,
+			kk: _.keys(glyph._path),
+			af: glyph._font._layoutEngine.getAvailableFeatures(),
+			k: _.keys(glyph._font)
+		};
+	}));
+	console.log('positions ---- ',lay.positions);
+	console.log('keys ----- ', _.keys(lay));
 
-	// // when adjusting glyphs, run through each lookuplist
-	// var lookupList = gpos.lookupList.items[0]; // it's a 'Struct'
-	// console.log('lookupType -- ', gposLookupType[lookupList.lookupType]);
-	// console.log('lookup list -- ', lookupList);
-
-	// // then run through each subtable of each lookuplist
-
-	// //console.log('lookup list, subtables -- ', lookupList.subTables);
-	// console.log('lookup list, coverage -- ', lookupList.subTables[0].coverage);
-	// console.log('lookup list, class records [0] -- ', lookupList.classRecords);
 }
+
+// function show_font_details(font) {
+// 	console.log('showing details of font');
+
+// 	var lg = fk.openSync(font);
+
+// 	var lay = lg.layout(example_text);
+
+// 	// console.log(lay.glyphs);
+
+// 	console.log('-------------------------------------------------------------');
+// 	for (var gid in lay.glyphs){
+// 		gid = gid * 1; // convert to int...
+// 		console.log('GLYPH =============', gid, lay.glyphs.length);
+// 		var glyph = lay.glyphs[gid];
+// 		var nextGlyphId = (gid < lay.glyphs.length) ? lay.glyphs[gid+1].id : null;
+// 		console.log(glyph.path.bbox);
+// 		console.log(glyph.id);
+// 		console.log(glyph._metrics);
+
+// 		console.log('FONT -=-=-=-=-=-=-=-');
+// 		var gpos = glyph._font._tables.GPOS;
+// 		console.log('GPOS : ', gpos);
+// 		var lookupList = gpos.lookupList.items[0];
+// 		for (var lookup of lookupList.subTables){
+// 			console.log('COVERAGE : ',lookup.coverage);
+// 			console.log('type: ', gposLookupType[lookupList.lookupType]);
+// 			console.log('lookup type: ', lookup.version);
+// 			console.log('lookup: ', convert_coverage(glyph.id, nextGlyphId, lookupList.lookupType, lookup.coverage, lookup));
+// 		}
+// 		// console.log('Lookups : ',thing._font._tables.GPOS);
+// 	}
+// 	console.log('-------------------------------------------------------------');
+
+// 	// https://www.microsoft.com/typography/otspec/gpos.htm
+
+// 	// returns a restructure/Struct
+// 	// not sure how to access the properties yet
+// 	// var thing = lay.glyphs[0]._font._tables.GPOS.lookupList;
+
+// 	// var tables = lay.glyphs[0]._font._tables;
+// 	// var gpos = tables.GPOS;
+
+// 	// var scriptList = gpos.scriptList;
+// 	// console.log('script list', scriptList);
+
+// 	// var featureList = gpos.featureList;
+// 	// console.log('feature list', featureList);
+// 	// console.log('feature list, lookup list indices', featureList[0].feature.lookupListIndexes);
+
+
+// 	// // lookup list tables
+// 	// // https://www.microsoft.com/typography/otspec/chapter2.htm
+
+// 	// // when adjusting glyphs, run through each lookuplist
+// 	// var lookupList = gpos.lookupList.items[0]; // it's a 'Struct'
+// 	// console.log('lookupType -- ', gposLookupType[lookupList.lookupType]);
+// 	// console.log('lookup list -- ', lookupList);
+
+// 	// // then run through each subtable of each lookuplist
+
+// 	// //console.log('lookup list, subtables -- ', lookupList.subTables);
+// 	// console.log('lookup list, coverage -- ', lookupList.subTables[0].coverage);
+// 	// console.log('lookup list, class records [0] -- ', lookupList.classRecords);
+// }
 
 fetch_fonts()
 	.then(function(fonts){
